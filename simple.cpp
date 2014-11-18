b7e10960c3d3e2d139546c0d8e73f4e566064280
Curtis <curtis@malainey.com>
2014-11-18 09:45:37 -0700

    Cleared comment and corrected addressing
    Shields start addressing from the top right, corrected for that

--- simple.cpp
+++ simple.cpp
@@ -1,23 +1,23 @@
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
 #include <Arduino.h>
 #include "Adafruit_NeoPixel.h"
 
-// Which pin on the Arduino is connected to the NeoPixels?
 #define PIN_SHIELD            6
 #define PIN_STICK_1           50
 #define PIN_STICK_2           51
 
-// How many NeoPixels are attached to the Arduino?
+#define POTENTIONMETER_PIN    10
+#define POTENTIONMETER_CHANGE 10
+
 #define NUMPIXELS_SHIELD      40
 
 #define NUMPIXELS_STICK       8
+
 #define SHIELD_DEPTH          8
 #define SHIELD_WIDTH          5
 
-const int amplitude = 10;
-
 Adafruit_NeoPixel shield = Adafruit_NeoPixel(NUMPIXELS_SHIELD, PIN_SHIELD, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stick1 = Adafruit_NeoPixel(NUMPIXELS_STICK, PIN_STICK_1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stick2 = Adafruit_NeoPixel(NUMPIXELS_STICK, PIN_STICK_2, NEO_GRB + NEO_KHZ800);
 
 typedef struct {
   uint8_t device;
@@ -31,12 +31,15 @@ pixel board[9][9];
 uint32_t colors[9];
 
 uint8_t focus_x,focus_y;
+uint16_t last_potentionmeter_level;
 
 void write_pixel(uint8_t x, uint8_t y, uint8_t color_id)
 {
-  if (board[x][y].device)
+  if (!board[x][y].device)
   {
     //write local
+    shield.setPixelColor(0, colors[0]);
+    shield.show();
   }
   else
   {
@@ -56,21 +59,35 @@ void check_focus()
   // start flashing new pixel
 }
 
+void check_color_change()
+{
+  if (analogRead(POTENTIONMETER_PIN) - last_potentionmeter_level > POTENTIONMETER_CHANGE || analogRead(POTENTIONMETER_PIN) - last_potentionmeter_level < -POTENTIONMETER_CHANGE)
+  {
+    last_potentionmeter_level = analogRead(POTENTIONMETER_PIN) - last_potentionmeter_level > POTENTIONMETER_CHANGE ? last_potentionmeter_level + POTENTIONMETER_CHANGE : last_potentionmeter_level - POTENTIONMETER_CHANGE;
+    if (!board[focus_x][focus_y].locked)
+    {
+
+    }
+  }
+}
+
 void setup() {
   shield.begin(); // This initializes the NeoPixel library.
   randomSeed(6798);
   Serial.begin(9600);
+  Serial1.begin(9600);
 
   // WHITE IS RESERVED FOR ERRORS
   colors[0] = shield.Color(20,2,12); //pink
   colors[1] = shield.Color(20,0,0); // Red
-  colors[2] = shield.Color(20,13,0); // orange
+  colors[2] = shield.Color(20,10,0); // orange
   colors[3] = shield.Color(20,20,0); // yellow
   colors[4] = shield.Color(0,20,0); // green
-  colors[5] = shield.Color(0,20,20); // cyan
-  colors[6] = shield.Color(0,0,20); // blue
-  colors[7] = shield.Color(20,0,20); // purple
-  colors[8] = shield.Color(10,10,10);  // grey
+  colors[5] = shield.Color(5,16,5);  // lime green
+  colors[6] = shield.Color(0,20,20); // cyan
+  colors[7] = shield.Color(0,0,20); // blue
+  colors[8] = shield.Color(10,0,20); // purple
+  
 
   // Define where all LEDS are
   // Define primary shield
@@ -80,9 +97,15 @@ void setup() {
     {
       board[width][depth].device = 0;
       board[width][depth].pin = 6;
-      board[width][depth].led = depth + SHIELD_DEPTH*width;
+      board[width][depth].led = 32 + depth - SHIELD_DEPTH*width;
     }
   }
+  shield.show();
+  for (int i = 0; i < 8; i++)
+  {
+    shield.setPixelColor(board[2][i].led, colors[i]);
+  }
+  shield.show();
   for (int width = SHIELD_WIDTH; width < 9; width++)
   {
     for (int depth = 0; depth < SHIELD_DEPTH; depth++)
@@ -95,8 +118,4 @@ void setup() {
 }
 
 void loop() {
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-  uint32_t color = shield.Color(255,255,255);
-  shield.setPixelColor(0, color);
-  shield.show();
 }
